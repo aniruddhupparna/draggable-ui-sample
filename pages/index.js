1f0b544971f6067e9be3c070061c6ef1882434d9
Aniruddh Upparna <aniruddhupparna@gmail.com>
2022-03-16 00:32:05 +0530

    added improvements to drag end event coordinate setting

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Image from 'next/image'
 import {useState, useEffect, useRef} from "react"
-import { ScatterChart, XAxis, YAxis, Scatter } from 'recharts';
+import { ScatterChart, XAxis, YAxis, Scatter, Cell } from 'recharts';
 import { getAnswers, postAnswers } from '../services/utils';
 
 // const data01 = [
@@ -19,13 +19,25 @@ export default function Home() {
   const [ctx, setCtx] = useState(null)
   const [answersData, setAnswersData] = useState([]);
   const pointRef = useRef(null)
+  const [maxAvgDistance, setMaxAvgDistance] = useState(336); // we assume this value based on the max distance between 2 points in our len = 300  height = 300 triangle
   const triangle = {a: { x: 0, y: 300 }, b: { x: 300, y: 300 }, c: { x: 150, y: 0 }}
   let coordinates = {x: 150, y: 150}
 
   useEffect(() => {
     setCtx(canvasRef.current.getContext("2d"));
-    getAnswers().then(data => setAnswersData(data))
+    getAnswers().then(data => addAvgDistance(data))
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+
+  function addAvgDistance(data) {
+    data.forEach(x => x.average_distance = data.map(y => distance(x, y)).reduce((a,b) => a + b, 0) / data.length)
+    setMaxAvgDistance(Math.max(...data.map(x => x.average_distance)))
+    setAnswersData(data)
+  }
+
+  function distance(pointA, pointB) {
+    return ((pointA.x-pointB.x)**2 + (pointB.y-pointB.y)**2)**0.5
+   }
 
   function handleStop (evt) {
     coordinates = {x: evt.pageX - canvasRef.current.offsetLeft, y: (evt.pageY - canvasRef.current.offsetTop - paddingTopOffset)}
@@ -36,12 +48,15 @@ export default function Home() {
       pointRef.current.style.top = `${canvasRef.current.offsetTop + 150}px`
       coordinates = {x: pointRef.current.style.left, y: pointRef.current.style.top}
     } else {
-      pointRef.current.style.left = `${evt.pageX}px`
-      pointRef.current.style.top = `${evt.pageY - paddingTopOffset}px`
+       // we subtract paddingTopOffset to account for the top padding we have given above canvas
+      // we subtract 16px to account for half the size of pointer itself
+      pointRef.current.style.left = `${evt.pageX - 16}px`
+      pointRef.current.style.top = `${evt.pageY - paddingTopOffset - 16}px`
     }
   };
 
   function ptInTriangle(p, p0, p1, p2) {
+    // check of point is inside triangle using barycentric coordinates
     var A = 1/2 * (-p1.y * p2.x + p0.y * (-p1.x + p2.x) + p0.x * (p1.y - p2.y) + p1.x * p2.y);
     var sign = A < 0 ? -1 : 1;
     var s = (p0.y * p2.x - p0.x * p2.y + (p2.y - p0.y) * p.x + (p0.x - p2.x) * p.y) * sign;
@@ -75,7 +90,7 @@ function render() {
 
 function savePos(e) {
   postAnswers(coordinates).then(data => {
-    getAnswers().then(res => setAnswersData(res))
+    getAnswers().then(res => addAvgDistance(res))
   })
 }
 
@@ -107,13 +122,17 @@ function savePos(e) {
             <polygon points="0 300, 300 300, 150 0" style={{fill:"whitesmoke"}}>
             </polygon>
           </svg>
-        <ScatterChart width={380} height={340}>
+        <ScatterChart width={370} height={335}>
           <XAxis dataKey="x" name="x"  type="number" domain={[0, 300]} axisLine={false} tick={false}/>
           <YAxis dataKey="y" name="y" reversed={true} type="number" domain={[0, 300]} axisLine={false} tick={false}/>
           {answersData.length > 0 && 
           <>
-          <Scatter name="selections" data={answersData.slice(0, -1)} fill="lightblue"/>
-          <Scatter name="last_selection" data={[answersData.slice(-1)[0]]} fill="royalblue"/>
+          <Scatter name="selections" data={answersData.slice(0, -1)}>
+              {answersData.slice(0, -1).map((entry, index) => (
+                  <Cell key={`cell-${index}`} fill={`rgba(0, 0, 255, ${(1 - entry.average_distance / maxAvgDistance)})`} />
+            ))}
+          </Scatter>
+          <Scatter name="last_selection" data={[answersData.slice(-1)[0]]} fill="red"/>
           </>}
         </ScatterChart>
         </div>
